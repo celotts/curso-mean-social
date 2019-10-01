4781fc9f4c3ab2bb9b156abd777a0a3a65a4542f
= <celotts@gmail.com>
2019-10-01 01:02:48 -0300

    Se agrega nueva funcionalidad

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var user_router = require("./routes/user");
 //Middlewares
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api", user_router)
--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,13 +3,13 @@
 var bcrypt = require("bcrypt-nodejs");
 var User = require("../model/user");
 
-function home(req, res){
+function home(req, res) {
     res.status(200).send({
         message: 'Ruta a la raiz'
     });
 }
 
-function pruebas (req, res){
+function pruebas(req, res) {
     res.status(200).send({
         message: 'Acción de prueba en el servidor Node.js'
     });
@@ -19,27 +19,27 @@ function saveUSer(req, res) {
     var params = req.body;
     var user = new User();
 
-    if(params.name && params.surname && params.nick && params.email && params.password) {
+    if (params.name && params.surname && params.nick && params.email && params.password) {
         user.name = params.name;
         user.surname = params.surname;
         user.nick = params.nick;
         user.email = params.email;
         user.role = 'ROLE_USER';
         User.image = null;
-        bcrypt.hash(params.password, null, null, (err, hash) =>{
+        bcrypt.hash(params.password, null, null, (err, hash) => {
             user.password = hash;
 
             user.save((err, userStored) => {
-                if(err) return res.status(500).send({message: "Error al guardar usuario"});
-                if(userStored) {
-                    res.status(200).send({user: userStored});
-                }else{
-                    res.status(400).send({message: "No se ha registrado el usuario"})
-                
+                if (err) return res.status(500).send({ message: "Error al guardar usuario" });
+                if (userStored) {
+                    res.status(200).send({ user: userStored });
+                } else {
+                    res.status(400).send({ message: "No se ha registrado el usuario" })
+
                 }
             })
         })
-    }else {
+    } else {
         res.status(200).send({
             message: "Envia todos los campos necesario"
         });
